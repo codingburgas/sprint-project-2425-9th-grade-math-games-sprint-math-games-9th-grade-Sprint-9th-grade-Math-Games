912ee616f984473b69b669e7efc63eae7adb02a4
git config --global user.name VVValchanova23 <VVValchanova23@codingburgas.bg>
2024-11-12 22:57:30 +0200

    Update menu

---
+++ Menu/Menu/Menu.h
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef MENU_H
+#define MENU_H
+
+#include "raylib.h"
+#include <stdlib.h>
+#include <time.h>
+// Screen dimensions
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 550
+
+// Structure for a button with hover effect
+struct Button {
+    Rectangle bounds;
+    const char* text;
+    Color normalColor;
+    Color hoverColor;
+    Color currentColor;
+};
+
+// Function declarations
+bool IsMouseOverButton(Button button);
+void DrawButton(Button button);
+
+#endif // MENU_H
