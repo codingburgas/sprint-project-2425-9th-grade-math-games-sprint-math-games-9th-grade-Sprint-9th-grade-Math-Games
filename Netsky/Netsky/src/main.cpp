043c3412736c88f873599c3abf199115537ff8bb
Петър Ангелов Димитров <padimitrov23@codingburgas.bg>
2024-11-12 21:16:34 +0200

    Add main menu

--- Netsky/Netsky/src/main.cpp
+++ Netsky/Netsky/src/main.cpp
@@ -1,22 +1,72 @@
-#include "raylib.h"
+#include <raylib.h>
+#include <iostream>
+#include "button.hpp"
+using namespace std;
 
-int main()
+int main() 
 {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
-
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
-
+    InitWindow(800, 800, "Main menu");
     SetTargetFPS(60);
+    InitAudioDevice();       
 
-    while (!WindowShouldClose())
+    Music music = LoadMusicStream("music/theme.mp3");
+
+    PlayMusicStream(music);
+
+    bool pause = false;             
+
+    Texture2D background = LoadTexture("graphics/Background.png");
+    Button startButton{"graphics/Play_button.png", {260, 300}, 0.2};
+    Button exitButton{"graphics/Quit_button.png", {260, 400}, 0.2};
+    Button muteButton{"graphics/Sound_button.png", {590, 690}, 0.15};
+    bool exit = false;
+    Font fontBm = LoadFontEx("fonts/CartoonCheck-Black.ttf", 32, 0, 250);
+ 
+
+    while (WindowShouldClose() == false && exit == false)
     {
+        UpdateMusicStream(music);
+
+        Vector2 mousePosition = GetMousePosition();
+        bool mousePressed = IsMouseButtonPressed(MOUSE_BUTTON_LEFT);
+
+        if(startButton.IsPressed(mousePosition, mousePressed))
+        {
+            cout << "**PLAY BUTTON IS PRESSED**";
+        }
+
+        if(exitButton.IsPressed(mousePosition, mousePressed))
+        {
+            exit = true;
+        }
+
+        if(muteButton.IsPressed(mousePosition, mousePressed))
+        {
+           StopMusicStream(music);
+           PlayMusicStream(music);
+        }
+
+        if(muteButton.IsPressed(mousePosition, mousePressed))
+        {
+            pause = !pause;
+
+            if (pause) 
+                PauseMusicStream(music);
+            else 
+                ResumeMusicStream(music);
+
+        }
+
         BeginDrawing();
-
-        ClearBackground(RAYWHITE);
-
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-
+        ClearBackground(BLACK);
+        DrawTexture(background, 0, 10, WHITE);
+        DrawTextEx(fontBm, "NETSKY", (Vector2{260, 150}), (float)fontBm.baseSize, 32, RAYWHITE);
+        DrawTextEx(fontBm, "minigames", { 190, 230},  (float)fontBm.baseSize, 32, RAYWHITE);
+        startButton.Draw();
+        exitButton.Draw();
+        muteButton.Draw();
         EndDrawing();
     }
+
+    CloseWindow();
 }
